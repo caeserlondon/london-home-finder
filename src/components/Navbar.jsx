a5f4d483a76647c0deb7eacd68238d65a7ce95a8
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-18 21:10:00 +0100

    Creating the nav bar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,83 @@
+import { useNavigate, useLocation } from 'react-router-dom'
+import { ReactComponent as ExploreIcon } from '../assets/svg/exploreIcon.svg'
+import { ReactComponent as OfferIcon } from '../assets/svg/localOfferIcon.svg'
+import { ReactComponent as PersonOutLineIcon } from '../assets/svg/personOutlineIcon.svg'
+
+const Navbar = () => {
+	let navigate = useNavigate()
+	let location = useLocation()
+
+	const pathMatchRoute = (route) => {
+		if (route === location.pathname) {
+			return true
+		}
+	}
+
+	return (
+		<footer className='navbar'>
+			<nav className='navbarNav'>
+				<ul className='navbarListItems'>
+					<li className='navbarListItem' onClick={() => navigate('/')}>
+						<ExploreIcon
+							className='exploreIcon'
+							fill={
+								pathMatchRoute('/')
+									? 'var(--primary-color)'
+									: 'var(--secondary-color)'
+							}
+						/>
+						<p
+							className={
+								pathMatchRoute('/')
+									? 'navbarListItemNameActive'
+									: 'navbarListItemName'
+							}
+						>
+							Explore
+						</p>
+					</li>
+					<li className='navbarListItem' onClick={() => navigate('/offers')}>
+						<OfferIcon
+							className='offerIcon'
+							fill={
+								pathMatchRoute('/offers')
+									? 'var(--primary-color)'
+									: 'var(--secondary-color)'
+							}
+						/>
+						<p
+							className={
+								pathMatchRoute('/offers')
+									? 'navbarListItemNameActive'
+									: 'navbarListItemName'
+							}
+						>
+							Offer
+						</p>
+					</li>
+					<li className='navbarListItem' onClick={() => navigate('/profile')}>
+						<PersonOutLineIcon
+							className='profileIcon'
+							fill={
+								pathMatchRoute('/profile')
+									? 'var(--primary-color)'
+									: 'var(--secondary-color)'
+							}
+						/>
+						<p
+							className={
+								pathMatchRoute('/profile')
+									? 'navbarListItemNameActive'
+									: 'navbarListItemName'
+							}
+						>
+							Profile
+						</p>
+					</li>
+				</ul>
+			</nav>
+		</footer>
+	)
+}
+
+export default Navbar
