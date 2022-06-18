251b8563e97960e3bb6d989d701b3f6b73427126
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-18 18:53:13 +0100

    Adding the Routes and the pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Explore from './pages/Explore'
+import ForgotPassword from './pages/ForgotPassword'
+import Login from './pages/LogIn'
+import Offers from './pages/Offers'
+import Profile from './pages/Profile'
+import SignUp from './pages/SignUp'
+
 function App() {
 	return (
 		<>
-			<h1>London Home Finder</h1>
+			<Router>
+				<Routes>
+					<Route path='/' element={<Explore />} />
+					<Route path='/offers' element={<Offers />} />
+					<Route path='/profile' element={<Profile />} />
+					<Route path='/log-in' element={<Login />} />
+					<Route path='/sign-up' element={<SignUp />} />
+					<Route path='/forgot-password' element={<ForgotPassword />} />
+				</Routes>
+			</Router>
 		</>
 	)
 }
