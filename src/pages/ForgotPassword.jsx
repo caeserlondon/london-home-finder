9ad5c1dcd2e11c19d6cb112ae2774d4f6f88e5bb
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-19 20:25:15 +0100

    Updating Forgoot Password Page

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,7 +1,58 @@
-import React from 'react'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth'
+import { toast } from 'react-toastify'
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
 
 const ForgotPassword = () => {
-	return <div>Forgot Password</div>
+	const [email, setEmail] = useState('')
+
+	const onChange = (e) => setEmail(e.target.value)
+
+	const onSubmit = async (e) => {
+		e.preventDefault()
+		try {
+			const auth = getAuth()
+
+			await sendPasswordResetEmail(auth, email)
+
+			toast.warn('Please check your email for a password reset link!')
+			toast.warn('please check your spam or junk mail folder')
+		} catch (error) {
+			toast.warn('Error sending email!')
+		}
+	}
+
+	return (
+		<div className='pageContainer'>
+			<header>
+				<p className='pageHeader'>Forgot Password</p>
+			</header>
+			<main>
+				<form onSubmit={onSubmit}>
+					<input
+						type='email'
+						placeholder='Email '
+						className='emailInput'
+						autoComplete='current-email'
+						value={email}
+						id='email'
+						onChange={onChange}
+					/>
+					<Link to='/log-in' className='forgotPasswordLink'>
+						Log In
+					</Link>
+					<div className='signInBar'>
+						<div className='signInText'>Send Reset Email</div>
+						<p>please check your spam or junk mail folder</p>
+						<button className='signInButton'>
+							<ArrowRightIcon className='arrowRightIcon' />
+						</button>
+					</div>
+				</form>
+			</main>
+		</div>
+	)
 }
 
 export default ForgotPassword
