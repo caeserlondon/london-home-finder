f19ff79fa72a8996c3d35e1ee90a5ba721bfb580
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-19 19:35:29 +0100

    Creating useAuthStatus hook

---
+++ src/hooks/useAuthStatus.js
@@ -0,0 +1,28 @@
+import { useState, useEffect, useRef } from 'react'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+
+export const useAuthStatus = () => {
+	const [loggedIn, setLoggedIn] = useState(false)
+
+	const [checkingStatus, setCheckingStatus] = useState(true)
+
+	// to avoid memory leaks
+	const isMounted = useRef(true)
+
+	useEffect(() => {
+		if (isMounted) {
+			const auth = getAuth()
+			onAuthStateChanged(auth, (user) => {
+				if (user) {
+					setLoggedIn(true)
+				}
+				setCheckingStatus(false)
+			})
+		}
+		return () => {
+			isMounted.current = false
+		}
+	}, [isMounted])
+
+	return { loggedIn, checkingStatus }
+}
