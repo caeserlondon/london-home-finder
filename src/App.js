b637721888c7d2eff831cfa95d072aeb35bc0f91
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-19 19:38:50 +0100

    Adding the Private Route & the Toast container

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import Explore from './pages/Explore'
 import ForgotPassword from './pages/ForgotPassword'
 import Login from './pages/LogIn'
@@ -6,6 +8,7 @@ import Offers from './pages/Offers'
 import Profile from './pages/Profile'
 import SignUp from './pages/SignUp'
 import Navbar from './components/Navbar'
+import PrivateRoute from './components/PrivateRoute'
 
 function App() {
 	return (
@@ -15,12 +18,15 @@ function App() {
 				<Routes>
 					<Route path='/' element={<Explore />} />
 					<Route path='/offers' element={<Offers />} />
-					<Route path='/profile' element={<Profile />} />
+					<Route path='/profile' element={<PrivateRoute />}>
+						<Route path='/profile' element={<Profile />} />
+					</Route>
 					<Route path='/log-in' element={<Login />} />
 					<Route path='/sign-up' element={<SignUp />} />
 					<Route path='/forgot-password' element={<ForgotPassword />} />
 				</Routes>
 			</Router>
+			<ToastContainer />
 		</>
 	)
 }
