1d3c47f7f8db7e2074393e8b7424448e0d8f00e4
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-20 21:06:39 +0100

    Adding the Create listing link to the Profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,8 +2,10 @@ import { useState } from 'react'
 import { getAuth, updateProfile } from 'firebase/auth'
 import { updateDoc, doc } from 'firebase/firestore'
 import { db } from '../firebase.config.js'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Link } from 'react-router-dom'
 import { toast } from 'react-toastify'
+import arrowRight from '../assets/svg/keyboardArrowRightIcon.svg'
+import homeIcon from '../assets/svg/homeIcon.svg'
 
 const Profile = () => {
 	const auth = getAuth()
@@ -90,6 +92,11 @@ const Profile = () => {
 						/>
 					</form>
 				</div>
+				<Link to='/create-listing' className='createListing'>
+					<img src={homeIcon} alt='home' />
+					<p>Sell or rent your property</p>
+					<img src={arrowRight} alt='go' />
+				</Link>
 			</main>
 		</div>
 	)
