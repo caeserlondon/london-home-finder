0488f6dbbfd199e8b760ef76f8ba151d74bdaf62
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-20 15:03:35 +0100

    Creating the Category page

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,5 +1,94 @@
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import {
+	collection,
+	getDocs,
+	query,
+	where,
+	orderBy,
+	limit,
+	startAfter,
+} from 'firebase/firestore'
+import { db } from '../firebase.config'
+import { toast } from 'react-toastify'
+import Spinner from '../components/Spinner'
+
 const Category = () => {
-	return <div>Category</div>
+	const [listings, setListings] = useState(null)
+
+	const [loading, setLoading] = useState(true)
+
+	const params = useParams()
+
+	useEffect(() => {
+		const fetchListings = async () => {
+			try {
+				// Get reference to the collection
+				const listingsRef = collection(db, 'listings')
+
+				// Create a query for the listtingsRef
+				const q = query(
+					listingsRef,
+					where('type', '==', params.categoryName),
+					orderBy('timestamp', 'desc'),
+					limit(10)
+				)
+
+				//Execute qury and get the listtings
+				const querySnap = await getDocs(q)
+
+				const listings = []
+
+				querySnap.forEach((doc) => {
+					// this will give me the Snapshot object that is the docRef.
+					// console.log(doc)
+
+					// using the date() method, I can get the date of the listings.
+					// console.log(doc.data())
+					// I can then use the date() method to get the date of the listings.
+
+					return listings.push({
+						id: doc.id,
+						data: doc.data(),
+					})
+				})
+
+				setListings(listings)
+				setLoading(false)
+			} catch (error) {
+				toast.error('Error fetching listings')
+			}
+		}
+
+		fetchListings()
+	}, [params.categoryName])
+
+	return (
+		<div className='category'>
+			<header>
+				<p className='pageHeader'>
+					{params.categoryName === 'sale'
+						? 'Places for sale'
+						: 'Places for rent'}
+				</p>
+			</header>
+			{loading ? (
+				<Spinner />
+			) : listings && listings.length > 0 ? (
+				<>
+					<main>
+						<ul className='categoryListings'>
+							{listings.map((listing) => (
+								<h3 key={listing.id}>{listing.data.name}</h3>
+							))}
+						</ul>
+					</main>
+				</>
+			) : (
+				<h2>No listings for {params.categoryName}</h2>
+			)}
+		</div>
+	)
 }
 
 export default Category
