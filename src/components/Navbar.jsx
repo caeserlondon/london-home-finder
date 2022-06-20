80c74c05516ec559d2a5e02c65a7d7985f0398cf
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-20 19:21:50 +0100

    Fixing a typo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -52,7 +52,7 @@ const Navbar = () => {
 									: 'navbarListItemName'
 							}
 						>
-							Offer
+							Offers
 						</p>
 					</li>
 					<li className='navbarListItem' onClick={() => navigate('/profile')}>
