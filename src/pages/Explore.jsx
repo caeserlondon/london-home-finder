6da87048bf3271d3ef9f565d1c1a45dde3b82f92
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-20 15:00:41 +0100

    Fixing a typo and Updating the header

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,12 +1,12 @@
 import { Link } from 'react-router-dom'
 import rentCategoryImage from '../assets/jpg/rentCategoryImage.jpg'
-import sellCategoryImage from '../assets/jpg/sellCategoryImage.jpg'
+import saleCategoryImage from '../assets/jpg/saleCategoryImage.jpg'
 
 const Explore = () => {
 	return (
 		<div className='explore'>
 			<header>
-				<p className='pageHeader'>Explore</p>
+				<p className='pageHeader'>Explore Our Listings</p>
 			</header>
 
 			<main>
@@ -22,10 +22,10 @@ const Explore = () => {
 						/>
 						<p className='exploreCategoryName'>Places for rent</p>
 					</Link>
-					<Link to={`/category/sell`}>
+					<Link to={`/category/sale`}>
 						<img
-							src={sellCategoryImage}
-							alt='sell'
+							src={saleCategoryImage}
+							alt='sale'
 							className='exploreCategoryImg'
 						/>
 						<p className='exploreCategoryName'>Places for sale</p>
