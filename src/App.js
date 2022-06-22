f3881cf0b4fa4fffa894b8369ff52d1493f861d9
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-22 17:56:49 +0100

    Adding the contact page & route to the App

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import SignUp from './pages/SignUp'
 import Category from './pages/Category'
 import CreateListing from './pages/CreateListing'
 import Listing from './pages/Listing'
+import Contact from './pages/contact'
 import Navbar from './components/Navbar'
 import PrivateRoute from './components/PrivateRoute'
 
@@ -29,6 +30,7 @@ function App() {
 					<Route path='/sign-up' element={<SignUp />} />
 					<Route path='/forgot-password' element={<ForgotPassword />} />
 					<Route path='/create-listing' element={<CreateListing />} />
+					<Route path='/contact/:landlordId' element={<Contact />} />
 					<Route
 						path='/category/:categoryName/:listingId'
 						element={<Listing />}
