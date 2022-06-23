64349f596c252ca35f8573738dc962f3fcb27c3b
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-23 20:02:40 +0100

    Adding the Slider component tro the explore page

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom'
 import rentCategoryImage from '../assets/jpg/rentCategoryImage.jpg'
 import saleCategoryImage from '../assets/jpg/saleCategoryImage.jpg'
+import Slider from '../components/Slider'
 
 const Explore = () => {
 	return (
@@ -10,8 +11,7 @@ const Explore = () => {
 			</header>
 
 			<main>
-				{/* Slider */}
-
+				<Slider />
 				<p className='exploreCategoryHeading'>Categories</p>
 				<div className='exploreCategories'>
 					<Link to={`/category/rent`}>
