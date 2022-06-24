78186972a2c9ee384ca3a791f86397f3aa4fd98e
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-24 21:41:28 +0100

    Adding the load more functunality

--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -19,6 +19,8 @@ const Offers = () => {
 
 	const [loading, setLoading] = useState(true)
 
+	const [lastFetchedListing, setLastFetchedListing] = useState(null)
+
 	const params = useParams()
 
 	const onDelete = () => {}
@@ -34,11 +36,14 @@ const Offers = () => {
 					listingsRef,
 					where('offer', '==', true),
 					orderBy('timestamp', 'desc'),
-					limit(10)
+					limit(6)
 				)
 
 				//Execute qury and get the listtings
 				const querySnap = await getDocs(q)
+
+				const lastVisible = querySnap.docs[querySnap.docs.length - 1]
+				setLastFetchedListing(lastVisible)
 
 				const listings = []
 
@@ -66,6 +71,43 @@ const Offers = () => {
 		fetchListings()
 	}, [])
 
+	// Load More listings from the database
+	const onFetchMoreListings = async () => {
+		try {
+			// Get reference
+			const listingsRef = collection(db, 'listings')
+
+			// Create a query
+			const q = query(
+				listingsRef,
+				where('offer', '==', true),
+				orderBy('timestamp', 'desc'),
+				startAfter(lastFetchedListing),
+				limit(6)
+			)
+
+			// Execute query
+			const querySnap = await getDocs(q)
+
+			const lastVisible = querySnap.docs[querySnap.docs.length - 1]
+			setLastFetchedListing(lastVisible)
+
+			const listings = []
+
+			querySnap.forEach((doc) => {
+				return listings.push({
+					id: doc.id,
+					data: doc.data(),
+				})
+			})
+
+			setListings((prevState) => [...prevState, ...listings])
+			setLoading(false)
+		} catch (error) {
+			toast.error('Could not fetch listings')
+		}
+	}
+
 	return (
 		<div className='category'>
 			<header>
@@ -87,6 +129,13 @@ const Offers = () => {
 							))}
 						</ul>
 					</main>
+					<br />
+					<br />
+					{lastFetchedListing && (
+						<p className='loadMore' onClick={onFetchMoreListings}>
+							Load More
+						</p>
+					)}
 				</>
 			) : (
 				<h2>There are no current offers</h2>
