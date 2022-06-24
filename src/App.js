ba5ce88b2a0433e4975fd186bad768f293ba215f
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-24 23:00:16 +0100

    Adding the edit listing page to the App

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Category from './pages/Category'
 import CreateListing from './pages/CreateListing'
 import Listing from './pages/Listing'
 import Contact from './pages/contact'
+import EditListing from './pages/EditListing'
 import Navbar from './components/Navbar'
 import PrivateRoute from './components/PrivateRoute'
 
@@ -30,6 +31,7 @@ function App() {
 					<Route path='/sign-up' element={<SignUp />} />
 					<Route path='/forgot-password' element={<ForgotPassword />} />
 					<Route path='/create-listing' element={<CreateListing />} />
+					<Route path='/edit-listing/:listingId' element={<EditListing />} />
 					<Route path='/contact/:landlordId' element={<Contact />} />
 					<Route
 						path='/category/:categoryName/:listingId'
