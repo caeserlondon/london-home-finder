c972df0aa2af77ed2c974b70a5f6934109da6fad
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-25 16:04:07 +0100

    Wrapping the page with the container

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -30,43 +30,45 @@ function Contact() {
 	const onChange = (e) => setMessage(e.target.value)
 
 	return (
-		<div className='pageContainer'>
-			<header>
-				<p className='pageHeader'>Contact Landlord</p>
-			</header>
+		<div className='mainContainer'>
+			<div className='pageContainer'>
+				<header>
+					<p className='pageHeader'>Contact Landlord</p>
+				</header>
 
-			{landlord !== null && (
-				<main>
-					<div className='contactLandlord'>
-						<p className='landlordName'>Contact {landlord?.name}</p>
-					</div>
-
-					<form className='messageForm'>
-						<div className='messageDiv'>
-							<label htmlFor='message' className='messageLabel'>
-								Message
-							</label>
-							<textarea
-								name='message'
-								id='message'
-								className='textarea'
-								value={message}
-								onChange={onChange}
-							></textarea>
+				{landlord !== null && (
+					<main>
+						<div className='contactLandlord'>
+							<p className='landlordName'>Contact {landlord?.name}</p>
 						</div>
 
-						<a
-							href={`mailto:${landlord.email}?Subject=${searchParams.get(
-								'listingName'
-							)}&body=${message}`}
-						>
-							<button type='button' className='primaryButton'>
-								Send Message
-							</button>
-						</a>
-					</form>
-				</main>
-			)}
+						<form className='messageForm'>
+							<div className='messageDiv'>
+								<label htmlFor='message' className='messageLabel'>
+									Message
+								</label>
+								<textarea
+									name='message'
+									id='message'
+									className='textarea'
+									value={message}
+									onChange={onChange}
+								></textarea>
+							</div>
+
+							<a
+								href={`mailto:${landlord.email}?Subject=${searchParams.get(
+									'listingName'
+								)}&body=${message}`}
+							>
+								<button type='button' className='primaryButton'>
+									Send Message
+								</button>
+							</a>
+						</form>
+					</main>
+				)}
+			</div>
 		</div>
 	)
 }
