470d545afe8e5b9ea67a715b5dce1355523cda3e
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-25 16:08:57 +0100

    Wrapping the page with the container

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -5,33 +5,35 @@ import Slider from '../components/Slider'
 
 const Explore = () => {
 	return (
-		<div className='explore'>
-			<header>
-				<p className='pageHeader'>Explore Our Listings</p>
-			</header>
+		<div className='mainContainer'>
+			<div className='explore'>
+				<header>
+					<p className='pageHeader'>Explore Our Listings</p>
+				</header>
 
-			<main>
-				<Slider />
-				<p className='exploreCategoryHeading'>Categories</p>
-				<div className='exploreCategories'>
-					<Link to={`/category/rent`}>
-						<img
-							src={rentCategoryImage}
-							alt='rent'
-							className='exploreCategoryImg'
-						/>
-						<p className='exploreCategoryName'>Places for rent</p>
-					</Link>
-					<Link to={`/category/sale`}>
-						<img
-							src={saleCategoryImage}
-							alt='sale'
-							className='exploreCategoryImg'
-						/>
-						<p className='exploreCategoryName'>Places for sale</p>
-					</Link>
-				</div>
-			</main>
+				<main>
+					<Slider />
+					<p className='exploreCategoryHeading'>Categories</p>
+					<div className='exploreCategories'>
+						<Link to={`/category/rent`}>
+							<img
+								src={rentCategoryImage}
+								alt='rent'
+								className='exploreCategoryImg'
+							/>
+							<p className='exploreCategoryName'>Places for rent</p>
+						</Link>
+						<Link to={`/category/sale`}>
+							<img
+								src={saleCategoryImage}
+								alt='sale'
+								className='exploreCategoryImg'
+							/>
+							<p className='exploreCategoryName'>Places for sale</p>
+						</Link>
+					</div>
+				</main>
+			</div>
 		</div>
 	)
 }
