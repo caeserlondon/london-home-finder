c439df1b4169d8bd8741a9c7a2ba9164b49009fe
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-25 22:40:29 +0100

    Updating the error message

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,5 +1,12 @@
 const Error = () => {
-	return <div>Error</div>
+	return (
+		<div className='mainContainer'>
+			<div className='errorPage'>
+				<h1>404</h1>
+				<h4>We are sorry, But the page you are looking for does not exist</h4>
+			</div>
+		</div>
+	)
 }
 
 export default Error
